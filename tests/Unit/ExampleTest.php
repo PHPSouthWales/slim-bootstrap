9a03c0605e2ed20a162de6a754778d55c8d464bd
Steve McDougall <juststeveking@gmail.com>
2018-07-12 14:15:15 +0100

    Adding in PHPUnit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * Example Test
+     *
+     * @test
+     *
+     * @return void
+     */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+}
